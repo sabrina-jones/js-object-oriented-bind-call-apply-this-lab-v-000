38e8e3089d5f85943c0bd246c3073b7df6e41a6a
Sabrina Jones <sabrina.jones87@gmail.com>
2017-12-13 15:53:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function justInvoke(fn){
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, arg){
+ return fn.call(thisValue, arg);
+}
+
+function setThisWithApply(fn, thisValue, args){
+  return fn.apply(thisValue, args);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  let copy = functionToBeCopied.bind(thisValue);
+  return copy;
+}
